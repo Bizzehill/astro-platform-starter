e5de84e318042eed373e896f174715a900f0edd8
Bizzehill <Steve.Berryhill@hotmail.com>
2025-03-29 23:22:32 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify';
 import react from '@astrojs/react';
-import tailwindcss from '@tailwindcss/vite';
+import netlify from '@astrojs/netlify';
+import tailwind from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        plugins: [tailwindcss()]
-    },
-    integrations: [react()],
-    adapter: netlify()
+  output: 'server',
+  adapter: netlify(),
+  integrations: [react()],
+  vite: {
+    plugins: [tailwind()]
+  },
 });
